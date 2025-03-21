19fb83b54a88ac793857281eb6f0672f39b6017a
KseniaLemetti <ksenia.lemetti@yandex.ru>
2025-03-21 13:18:43 +0300

    Add new files

---
+++ макет1/scripts/script.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const sectionId = this.getAttribute('href');
+            const section = document.querySelector(sectionId);
+
+            section.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
